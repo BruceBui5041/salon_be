02cd30eccd4369ef11ac13ad450a9c9deb35d38e
Bui Hong Lan <lan@hydrax.io>
2024-10-27 14:20:39 +0000

    Make UserID and ServiceID fields unexported in JSON serialization

--- model/image.go
+++ model/image.go
@@ -16,9 +16,9 @@ func init() {
 
 type Image struct {
 	common.SQLModel `json:",inline"`
-	UserID          uint32            `json:"user_id" gorm:"column:user_id;index"`
+	UserID          uint32            `json:"-" gorm:"column:user_id;index"`
 	User            *User             `json:"user,omitempty" gorm:"foreignKey:UserID;references:Id;constraint:OnDelete:SET NULL;"`
-	ServiceID       uint32            `json:"service_id" gorm:"column:service_id;index"`
+	ServiceID       uint32            `json:"-" gorm:"column:service_id;index"`
 	Service         *Service          `json:"service,omitempty" gorm:"foreignKey:ServiceID;references:Id;constraint:OnDelete:SET NULL;"`
 	URL             string            `json:"url" gorm:"column:url;type:text"`
 	ServiceVersions []*ServiceVersion `json:"service_versions,omitempty" gorm:"many2many:service_version_images;foreignKey:Id;joinForeignKey:ImageID;References:Id;joinReferences:ServiceVersionID;constraint:OnDelete:CASCADE;"`
