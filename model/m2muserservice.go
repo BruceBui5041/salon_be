16d9a9ef293e2afd824347a279b51309c8bccb3a
Bui Hong Lan <lan@hydrax.io>
2025-02-13 11:59:26 +0000

    fixbug query service men for service version

--- model/m2muserservice.go
+++ model/m2muserservice.go
@@ -1,8 +1,14 @@
 package models
 
+func init() {
+	// modelhelper.RegisterModel(UserService{})
+}
+
 type UserService struct {
-	UserID           uint32 `json:"user_id" gorm:"primaryKey;column:user_id;joinReferences:UserID"`
-	ServiceVersionID uint32 `json:"service_version_id" gorm:"primaryKey;column:service_version_id;joinReferences:ServiceVersionID"`
+	UserID           uint32          `json:"-" gorm:"primaryKey;column:user_id"`
+	ServiceVersionID uint32          `json:"-" gorm:"primaryKey;column:service_version_id"`
+	User             *User           `json:"user,omitempty" gorm:"foreignKey:UserID;references:Id"`
+	ServiceVersion   *ServiceVersion `json:"service_version,omitempty" gorm:"foreignKey:ServiceVersionID;references:Id"`
 }
 
 func (UserService) TableName() string {
